4c58eea5072e35f2839be3407a94b5507ee6ffe2
emma <e.r.southall@warwick.ac.uk>
2017-11-10 14:13:07 +0000

    workingcode

--- question1.jl
+++ question1.jl
@@ -1,10 +1,9 @@
 function recursive_fp(type_precision)
     a_seq = zeros(type_precision, 80)
     a_seq[1] = 1
-    a_seq[2] = 2/3
+    a_seq[2] = type_precision(2)/type_precision(3)
     for i = 2:79
-        a_seq[i+1] = 2*a_seq[i] - (8/9)*a_seq[i-1]
+        a_seq[i+1] = 2*a_seq[i] - (type_precision(8)/type_precision(9))*a_seq[i-1]
     end
     return a_seq
 end
-           
